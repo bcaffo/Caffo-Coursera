d802d6a92dd3bf64c6140efbf4b0c0de27e90c8b
Brian Caffo <bcaffo@gmail.com>
2012-06-26 20:59:59 -0400

    Initial commit

---
+++ exponential.R
@@ -0,0 +1,12 @@
+#postscript("exponential.eps", width=4, height=4, horizontal = FALSE)
+xvals <- seq(0, 20, length = 1000) 
+plot(xvals, dexp(xvals, 1/5), xlab = "Survival time in years",
+     ylab = "density",
+     frame = FALSE,
+     type = "l")
+polygon(c(xvals[xvals >= 6], rev(xvals[xvals >= 6])),
+        c(dexp(xvals[xvals >= 6], 1/5), rep(0, sum(xvals >= 6))),
+        col = grey(.5)
+        )
+#dev.off()
+
